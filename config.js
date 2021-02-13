5cb14b815fad449e9eb706c999efed7455e9b753
Vibhu3 <65866042+Vibhu3@users.noreply.github.com>
2021-02-12 20:07:33 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB_9u7BEFUMxcSfaEzl54ybJiSIbDu5Tpw",
+    authDomain: "bedtime-e4688.firebaseapp.com",
+    databaseURL: "https://bedtime-e4688-default-rtdb.firebaseio.com",
+    projectId: "bedtime-e4688",
+    storageBucket: "bedtime-e4688.appspot.com",
+    messagingSenderId: "26520124017",
+    appId: "1:26520124017:web:c4667d70164de5d7a179d0"
+  };
